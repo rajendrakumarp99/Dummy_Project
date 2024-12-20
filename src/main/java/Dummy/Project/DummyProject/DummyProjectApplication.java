2fb7a62daef0cd83374495e06bf3b6625796698e
Rajendrakumar p <rajendrakumarp99@gmail.com>
2024-12-20 12:28:58 +0530

    first commit

---
+++ src/main/java/Dummy/Project/DummyProject/DummyProjectApplication.java
@@ -0,0 +1,13 @@
+package Dummy.Project.DummyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyProjectApplication.class, args);
+	}
+
+}
